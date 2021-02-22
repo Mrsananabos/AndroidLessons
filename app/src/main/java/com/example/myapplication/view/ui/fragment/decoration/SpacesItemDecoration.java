02ed5d009d9b788f608d878d7e66d720fda2cd69
Shveytser-A <Shveytser-A@gaz-is.ru>
2021-02-22 11:38:47 +0300

    add item decoration to recycleview menu item

---
+++ app/src/main/java/com/example/myapplication/view/ui/fragment/decoration/SpacesItemDecoration.java
@@ -0,0 +1,29 @@
+package com.example.myapplication.view.ui.fragment.decoration;
+
+import android.graphics.Rect;
+import android.view.View;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SpacesItemDecoration extends RecyclerView.ItemDecoration {
+    private int space;
+
+    public SpacesItemDecoration(int space) {
+        this.space = space;
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view,
+                               RecyclerView parent, RecyclerView.State state) {
+        outRect.left = space;
+        outRect.right = space;
+        outRect.bottom = space;
+        outRect.top = space;
+
+        if (parent.getChildLayoutPosition(view) == 0 || parent.getChildLayoutPosition(view) == 1) {
+            outRect.top = 0;
+        } else {
+            outRect.top = space;
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/myapplication/view/ui/fragment/menuItems/FragmentMenuItem.java
@@ -0,0 +1,54 @@
+package com.example.myapplication.view.ui.fragment.menuItems;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.myapplication.R;
+import com.example.myapplication.model.MusicGenreData;
+import com.example.myapplication.view.adapter.AdapterMusicMenuItems;
+import com.example.myapplication.view.ui.fragment.decoration.SpacesItemDecoration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FragmentMenuItem extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_music_menu, null);
+        initViews(view);
+        return view;
+    }
+
+    private void initViews(View view) {
+        initList(view);
+    }
+
+    private void initList(View view) {
+        RecyclerView rvItems = view.findViewById(R.id.rvItems);
+        rvItems.setLayoutManager(new GridLayoutManager(view.getContext(), 2));
+        rvItems.addItemDecoration(new SpacesItemDecoration(30));
+        AdapterMusicMenuItems adapter = new AdapterMusicMenuItems();
+        rvItems.setAdapter(adapter);
+        adapter.updateData(getData());
+    }
+
+    private List<MusicGenreData> getData() {
+        ArrayList<MusicGenreData> result = new ArrayList<>();
+        result.add(new MusicGenreData("Pop", "", ""));
+        result.add(new MusicGenreData("Рок", "", ""));
+        result.add(new MusicGenreData("Рэп", "", ""));
+        result.add(new MusicGenreData("Кантри", "", ""));
+        result.add(new MusicGenreData("Джаз", "", ""));
+        result.add(new MusicGenreData("Хип-хоп", "", ""));
+        return result;
+    }
+
+}
